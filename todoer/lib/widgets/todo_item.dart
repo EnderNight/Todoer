e7b9f87bad0aa0055fd7bc51660ff1033ecd49ef
EnderNight <ndxendernight@gmail.com>
2023-12-14 15:57:50 +0100

    Changed bin icon display, changed app name

--- todoer/lib/widgets/todo_item.dart
+++ todoer/lib/widgets/todo_item.dart
@@ -20,7 +20,9 @@ class TodoItemWidget extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(
-            color: Theme.of(context).primaryColorLight,
+            color: isDone
+                ? Theme.of(context).hoverColor
+                : Theme.of(context).primaryColorLight,
             borderRadius: const BorderRadius.all(Radius.circular(5))),
         child: GestureDetector(
           behavior: HitTestBehavior.opaque,
@@ -37,14 +39,35 @@ class TodoItemWidget extends StatelessWidget {
                           : TextDecoration.none),
                 ),
               ),
-              IconButton(
-                onPressed: onDeletePress,
-                icon: const Icon(Icons.delete),
+              BinButton(
+                onDeletePress: onDeletePress,
+                isDone: isDone,
               )
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class BinButton extends StatelessWidget {
+  const BinButton({
+    super.key,
+    required this.onDeletePress,
+    required this.isDone,
+  });
+
+  final void Function() onDeletePress;
+  final bool isDone;
+
+  @override
+  Widget build(BuildContext context) {
+    return isDone
+        ? IconButton(
+            onPressed: onDeletePress,
+            icon: const Icon(Icons.delete),
+          )
+        : const SizedBox.shrink();
   }
 }
